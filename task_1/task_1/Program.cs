6e1e2ac24cb0e70608a51362ab2015ec5b4b471d
ARMEN0707 <vorotynskiy.gleb@mail.ru>
2021-08-18 17:51:56 +0300

    add solution task 1

---
+++ task_1/task_1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("en-us");
+            try
+            {
+                using (StreamReader sr = new StreamReader("InputData.txt"))
+                {
+                    string str;
+                    while (!sr.EndOfStream)
+                    {
+                        str = sr.ReadLine();
+                        string[] numbers = str.Split(new char[] { ',' });
+
+                        if (numbers.Length == 2)
+                        {
+                            bool isCorrect = true;
+                            foreach (string number in numbers)
+                            {
+                                double num;
+                                if (!double.TryParse(number, NumberStyles.AllowDecimalPoint, culture, out num))
+                                {
+                                    isCorrect = false;
+                                    Console.WriteLine("Invalid data " + str);
+                                    break;
+                                }
+                            }
+                            if (isCorrect)
+                                Console.WriteLine("X: " + numbers[0] + " Y: " + numbers[1]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid data " + str);
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("File is not find.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
