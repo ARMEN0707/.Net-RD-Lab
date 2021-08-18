f186b13a6b44bf379c19aa194d892a49b529bd25
ARMEN0707 <vorotynskiy.gleb@mail.ru>
2021-08-19 01:36:00 +0300

    add solution task_2

---
+++ task_2/Zad_1/Zad_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Zad_1
+{
+    class RootNumber
+    {
+        private double _eps  = 0.0001;
+
+        public double Eps
+        {
+            get { return _eps; }
+            set { _eps = value < 0 ? _eps = 0 : _eps = value; }
+        }
+
+        public RootNumber(double eps)
+        {
+            Eps = eps;
+        }
+
+        public double Sqrt(double number, double degree)
+        {
+            double x0 = number / degree;            
+            double x1 = GetNumber(number, degree, x0);
+
+            while (Math.Abs(x1 - x0) > Eps)
+            {
+                x0 = x1;
+                x1 = GetNumber(number, degree, x0);
+            }
+
+            return x1;
+        }
+
+        private double GetNumber(double number, double degree, double x0) => (1 / degree) * ((degree - 1) * x0 + number / Math.Pow(x0, degree - 1));
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+            double degree;
+            double eps;
+            Console.Write("Enter number: ");            
+            number = GetNumber();
+            Console.Write("Enter degree: ");
+            degree = GetNumber();
+            Console.Write("Enter eps: ");
+            eps = GetNumber();
+
+            RootNumber rn = new RootNumber(eps);
+            double result = rn.Sqrt(number, degree);
+            double resultPow = Math.Pow(result, degree);
+            if (Math.Abs(number - resultPow) < rn.Eps)
+            {
+                Console.WriteLine("Result: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Result don't equals Math.Pow()");
+            }
+            Console.ReadKey();
+        }
+
+        static double GetNumber()
+        {
+            double number;
+            string str = Console.ReadLine();
+            while (!double.TryParse(str, out number))
+            {
+                Console.WriteLine("Invalid data. For example: 1,56 or 2");
+                str = Console.ReadLine();
+            }
+            return number;
+        }
+    }
+}
---
+++ task_2/Zad_2/Zad_2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = GetNumber();
+
+            Console.WriteLine("Result: " + Convert(number));
+
+            Console.ReadLine();
+        }
+
+        static int GetNumber()
+        {
+            int  number;
+            string str = Console.ReadLine();
+            while (!int.TryParse(str, out number))
+            {
+                Console.WriteLine("Invalid data. For example: 2");
+                str = Console.ReadLine();
+            }
+            return number;
+        }
+        static string Convert(int number)
+        {
+            string str = "";
+            
+            while(number > 1)
+            {
+                int remainder = number % 2;
+                number /= 2;
+                str += remainder;
+            }
+
+            str += number;
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+        }
+    }
+}
