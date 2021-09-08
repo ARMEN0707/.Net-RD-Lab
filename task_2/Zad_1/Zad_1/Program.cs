c4a3f4da9e5e97fcdbe048f63904dc00388156ea
ARMEN0707 <vorotynskiy.gleb@mail.ru>
2021-09-08 23:25:01 +0300

    create individual files for type and edit naming

--- task_2/Zad_1/Zad_1/Program.cs
+++ task_2/Zad_1/Zad_1/Program.cs
@@ -2,61 +2,24 @@
 
 namespace Zad_1
 {
-    struct RootNumber
-    {
-        private readonly double _eps;
-
-        public double Eps
-        {
-            get { return _eps; }
-        }
-
-        public RootNumber(double eps)
-        {
-            _eps = eps;
-        }
-
-        public double Sqrt(double number, double degree)
-        {
-            double x0 = number / degree;            
-            double x1 = GetValue(number, degree, x0);
-
-
-            while (Math.Abs(x1 - x0) > Eps)
-            {
-                x0 = x1;
-                x1 = GetValue(number, degree, x0);
-            }
-
-            double resultPow = Math.Pow(x1, degree);
-
-            if (Math.Abs(number - resultPow) < Eps)
-                return x1;               
-            else
-                throw new ArgumentException("Result don't equals Math.Pow()");
-        }
-
-        private double GetValue(double number, double degree, double x0) => (1 / degree) * ((degree - 1) * x0 + number / Math.Pow(x0, degree - 1));
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             double number;
             double degree;
-            double eps;
+            double epsilon;
             Console.Write("Enter number: ");            
             number = GetNumber();
             Console.Write("Enter degree: ");
             degree = GetNumber();
             Console.Write("Enter eps: ");
-            eps = GetNumber();
+            epsilon = GetNumber();
 
             try
             {
-                RootNumber rn = new RootNumber(eps);
-                double result = rn.Sqrt(number, degree);
+                MethodNewton methodNewton = new MethodNewton(epsilon);
+                double result = methodNewton.Sqrt(number, degree);
                 Console.WriteLine("Result: " + result);
             }
             catch(ArgumentException ex)
--- task_2/Zad_2/Zad_2/Program.cs
+++ task_2/Zad_2/Zad_2/Program.cs
@@ -2,33 +2,13 @@
 
 namespace Zad_2
 {
-    struct NumberConvertToBinaryCode
-    {
-        public string Convert(int number)
-        {
-            string str = "";
-
-            while (number > 1)
-            {
-                int remainder = number % 2;
-                number /= 2;
-                str += remainder;
-            }
-
-            str += number;
-            char[] arr = str.ToCharArray();
-            Array.Reverse(arr);
-            return new string(arr);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
             Console.Write("Enter number: ");
             int number = GetNumber();
-            NumberConvertToBinaryCode convertor = new NumberConvertToBinaryCode();
+            ConvertorNumber convertor = new ConvertorNumber();
 
             Console.WriteLine("Result: " + convertor.Convert(number));
 
@@ -45,7 +25,6 @@ namespace Zad_2
                 str = Console.ReadLine();
             }
             return number;
-        }
-        
+        }        
     }
 }
