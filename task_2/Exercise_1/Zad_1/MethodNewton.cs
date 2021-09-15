38cbfe73fc7a77e98d3295c0367996335cd0f586
ARMEN0707 <vorotynskiy.gleb@mail.ru>
2021-09-15 19:37:28 +0300

    hotfix

---
+++ task_2/Exercise_1/Zad_1/MethodNewton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad_1
+{   
+    class MethodNewton
+    {
+        public double Epsilon
+        {
+            get;
+            private set;
+        }
+
+        public MethodNewton(double epsilon)
+        {
+            Epsilon = epsilon;
+        }
+
+        public double Sqrt(double number, double degree)
+        {
+            double argument = number / degree;
+            double functionValue = GetFunctionValue(number, degree, argument);
+
+
+            while (Math.Abs(functionValue - argument) > Epsilon)
+            {
+                argument = functionValue;
+                functionValue = GetFunctionValue(number, degree, argument);
+            }
+
+            double resultPow = Math.Pow(functionValue, degree);
+
+            if (Math.Abs(number - resultPow) < Epsilon)
+                return functionValue;
+            else
+                throw new ArgumentException("Result don't equals Math.Pow()");
+        }
+
+        private double GetFunctionValue(double number, double degree, double argument) 
+            => (1 / degree) * ((degree - 1) * argument + number / Math.Pow(argument, degree - 1));
+    }
+}
---
+++ task_2/Exercise_2/Zad_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Zad_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = GetNumber();
+            var convertor = new ConvertorNumber();
+
+            Console.WriteLine("Result: " + convertor.Convert(number));
+
+            Console.ReadLine();
+        }
+
+        static int GetNumber()
+        {
+            int  number;
+            string str = Console.ReadLine();
+            while (!int.TryParse(str, out number))
+            {
+                Console.WriteLine("Invalid data. For example: 2");
+                str = Console.ReadLine();
+            }
+            return number;
+        }        
+    }
+}
